45e80e9e6a33cc16b0ac8f253870624131899969
Vincentius Kevin <kevincentius@yahoo.com>
2019-05-04 21:22:36 +0200

    search depth

--- main.py
+++ main.py
@@ -7,36 +7,28 @@ from random import randint
 
 # Game Rules
 game = Game()
-game.num_colors = 3
+game.num_colors = 6
 
 evaluator = CountEvaluator()
 tree_search = TreeSearch()
-
-# baseline: 1-deep count evaluator --> 843.3875, 898.015625 moves (3 colors, 9x9 board)
-# untrained: 167.39, 123.88, 129.17, 111.58 moves
 
 wa = 0
 num_moves = []
 for e in range(1000):
 	game.reset()
 
-	for n_moves in range(2000):
-		best_move, best_tile, best_score, legal_moves = tree_search.find_best_move(game, evaluator, 1)
-		#target_pos, tile, score, legal_moves = tree_search.find_best_move(game, agent, 1)
+	for n_moves in range(1000):
+		best_move, best_tile, best_score, legal_moves = tree_search.find_best_move(game, evaluator, 4)
 
 		if len(legal_moves) > 0:
 			game.play(best_move, best_tile)
 
 		else:
-			num_moves.append(game.num_moves)
-			wa = wa * 0.95 + 0.05 * game.num_moves
-
-			# TODO: just bug check, can be removed
-			if game.num_moves < 4:
-				game.print()
-				input()
 			break
 	
+	num_moves.append(game.num_moves)
+	wa = wa * 0.95 + 0.05 * game.num_moves
+
 	print('round: {}, wa: {:.2f}, moves: {}'.format(
 		e, wa, game.num_moves
 	))
